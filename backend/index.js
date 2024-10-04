d8237b3ca6ee03befc13939cc3acfd67aeb955bc
Shruti Peragani <sangeetaperagani8642@gmail.com>
2024-10-04 14:57:06 +0530

    changing the url's

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://quickhire-3epm.onrender.com",
   credentials: true,
 };
 
